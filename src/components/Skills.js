354de5495b6b7153eb3caa559312ae3490333b13
pradum9112 <pradumsonkar9112@gmail.com>
2026-09-19 19:13:16 +0530

    updated skill

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,14 +3,12 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { Container, Row, Col } from "react-bootstrap";
 import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import colorSharp from "../assets/img/color-sharp.png";
 
 function Skills() {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
     },
@@ -27,6 +25,7 @@ function Skills() {
       items: 1,
     },
   };
+
   return (
     <section className="skill" id="skills">
       <Container>
@@ -40,35 +39,35 @@ function Skills() {
                 className="skill-slider"
               >
                 <div className="item">
-                  <img src={meter1} alt="Image" />
+                  <img src={meter1} alt="Software Developer skill level" />
                   <h5>Software Developer</h5>
                 </div>
                 <div className="item">
-                  <img src={meter1} alt="Image" />
+                  <img src={meter1} alt="Communication Skills level" />
                   <h5>Communication Skills</h5>
                 </div>
                 <div className="item">
-                  <img src={meter3} alt="Image" />
+                  <img src={meter3} alt="DSA skill level" />
                   <h5>DSA</h5>
                 </div>
                 <div className="item">
-                  <img src={meter1} alt="Image" />
+                  <img src={meter1} alt="Full Stack Developer skill level" />
                   <h5>Full Stack Developer</h5>
                 </div>
                 <div className="item">
-                  <img src={meter1} alt="Image" />
+                  <img src={meter1} alt="MERN Stack Developer skill level" />
                   <h5>MERN Stack Developer</h5>
                 </div>
                 <div className="item">
-                  <img src={meter1} alt="Image" />
+                  <img src={meter1} alt="Backend Developer skill level" />
                   <h5>Backend Developer</h5>
                 </div>
                 <div className="item">
-                  <img src={meter1} alt="Image" />
+                  <img src={meter1} alt="Frontend Developer skill level" />
                   <h5>Frontend Developer</h5>
                 </div>
                 <div className="item">
-                  <img src={meter3} alt="Image" />
+                  <img src={meter3} alt="Google Cloud Platform skill level" />
                   <h5>Google Cloud Plateform(GCP)</h5>
                 </div>
               </Carousel>
@@ -76,7 +75,7 @@ function Skills() {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-left" src={colorSharp} />
+      <img className="background-image-left" src={colorSharp} alt="" />
     </section>
   );
 }
